10b6f790c3ddccc6358dfe90fe66adfb417a5292
Esa Auranen <esa.auranen@iki.fi>
2018-09-09 13:45:47 +0300

    Add Esc press to hide the editor

--- js/editor.js
+++ js/editor.js
@@ -1,6 +1,18 @@
 
 $(function () {
+
    $('#closebutton').click(function () {
       $('#tournamenteditor').css('display', 'none');
    });
+
+    window.addEventListener('keydown', function(event) {
+         if (event.key === 'Escape') {
+             let visible = $('#tournamenteditor').css('display') === 'block';
+
+             if (visible) {
+                 $('#tournamenteditor').css('display', 'none');
+             }
+
+         }
+    }, true);
 });
